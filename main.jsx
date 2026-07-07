8bc5a0bdf798da0f2e094f3267850c0bd47187e7
adhikarimanohard-design <adhikarimanohard@gmail.com>
2026-07-08 02:06:10 +0530

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CodeForgeAI from './index.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CodeForgeAI />
+  </React.StrictMode>
+);
